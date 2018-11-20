4b15359c9b6820188a655a19ce7898691fc5baed
karen ercoli <karenercoli@gmail.com>
2018-11-20 19:47:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ if array.count == 1
 elsif array.count == 2
   array.join(" and ")
 elsif array.count == 3
-  array.join(", ")
+  array[0..-2].join(", ") + " and " + array[-1]
 elsif array.count >3
   array.join(", ")
 end
